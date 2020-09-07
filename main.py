d8a2ee79b36f8098273ba692a497445ae8e28396
Yu <tuba0927hy@gmail.com>
2020-09-07 15:45:18 +0900

    feat: add display a message

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
+print("回数を入力")
 n = int(input())
+print("最小値を入力")
 start = float(input())
+print("最大値を入力")
 end = float(input())
 for i in range(n):
     l = [random.uniform(start,end)]

771e36d51908c59062f757624caa6593176cb4d8
Yu <tuba0927hy@gmail.com>
2020-09-07 15:48:12 +0900

    feat: add to make it easier to see

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ print("最小値を入力")
 start = float(input())
 print("最大値を入力")
 end = float(input())
+print("----------")
 for i in range(n):
     l = [random.uniform(start,end)]
     for j in l:
         print(round(j,1))
+print("----------")

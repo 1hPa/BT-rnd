5318dd81b2a3fc7165e490b21ec69d41043d86e1
Yu <tuba0927hy@gmail.com>
2020-09-07 15:32:01 +0900

    feat: add start and end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 n = int(input())
+start = float(input())
+end = float(input())
 for i in range(n):
-    l = [random.uniform(36.0,37.0)]
+    l = [random.uniform(start,end)]
     for j in l:
         print(round(j,1))
